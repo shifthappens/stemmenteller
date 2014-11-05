043aeb2fc984dc714aee4436be0aeb78cd4de642
Coen de Jong <coen@shifthappens.nl>
2014-11-05 13:53:37 +0100

    font problems in IE..

--- application/controllers/Rankings.php
+++ application/controllers/Rankings.php
@@ -68,7 +68,7 @@ class Rankings extends CI_Controller {
 
 	public function makepwd()
 	{
-		echo password_hash('nff35 leeuwarden', PASSWORD_BCRYPT);
+		echo password_hash('nff35 leeuwarden admin', PASSWORD_BCRYPT);
 	}
 
 	public function xml()
@@ -80,60 +80,60 @@ class Rankings extends CI_Controller {
 	}
 
 	//change to public to enable import functionality
-	private function import()
-	{
-		$f = file_get_contents('/Users/RidderGraniet/Sites/stemmenteller/lijst.csv');
-		$this->load->library('parsecsv', NULL, 'csv');
-		$this->csv->delimiter = ';';
-		$this->csv->input_encoding = "UTF-8";
-		$this->csv->parse($f);
-		//echo "<pre>".print_r($this->csv->data, true)."</pre>";
+	// private function import()
+	// {
+	// 	$f = file_get_contents('/Users/RidderGraniet/Sites/stemmenteller/lijst.csv');
+	// 	$this->load->library('parsecsv', NULL, 'csv');
+	// 	$this->csv->delimiter = ';';
+	// 	$this->csv->input_encoding = "UTF-8";
+	// 	$this->csv->parse($f);
+	// 	//echo "<pre>".print_r($this->csv->data, true)."</pre>";
 
-		$this->load->model('Movies_model');
+	// 	$this->load->model('Movies_model');
 
-		$movies = array();
-		$showings = array();
+	// 	$movies = array();
+	// 	$showings = array();
 
-		foreach($this->csv->data as $key => $entry)
-		{
-			//don't do this one if noentry is set
-			if(trim($entry['noentry']) == 'x')
-				continue;
+	// 	foreach($this->csv->data as $key => $entry)
+	// 	{
+	// 		//don't do this one if noentry is set
+	// 		if(trim($entry['noentry']) == 'x')
+	// 			continue;
 
-			//movie itself
-			if(strpos($entry['prize'], 'ja') !== FALSE)
-				$entry['movie_can_win'] = 1;
-			else
-				$entry['movie_can_win'] = 0;
+	// 		//movie itself
+	// 		if(strpos($entry['prize'], 'ja') !== FALSE)
+	// 			$entry['movie_can_win'] = 1;
+	// 		else
+	// 			$entry['movie_can_win'] = 0;
 
-			$movie = array(
-				'movie_name' => $entry['Titel film'],
-				'movie_can_win' => $entry['movie_can_win']
-				);
+	// 		$movie = array(
+	// 			'movie_name' => $entry['Titel film'],
+	// 			'movie_can_win' => $entry['movie_can_win']
+	// 			);
 
 
-			if(!empty(trim($entry['Tijd1'])))
-			{
-				$date = explode('-', $entry['Datum1']);
-				$movie['movie_showings'][0]['showing_datetime'] = strtotime($date[2].'-'.$date[1].'-'.$date[0].' '.$entry['Tijd1']);
-			}
+	// 		if(!empty(trim($entry['Tijd1'])))
+	// 		{
+	// 			$date = explode('-', $entry['Datum1']);
+	// 			$movie['movie_showings'][0]['showing_datetime'] = strtotime($date[2].'-'.$date[1].'-'.$date[0].' '.$entry['Tijd1']);
+	// 		}
 
-			if(!empty(trim($entry['tijd2'])))
-			{
-				$movie['movie_showings'][1]['showing_datetime'] = strtotime($entry['datum2'].'-14 '.$entry['tijd2']);
-			}
+	// 		if(!empty(trim($entry['tijd2'])))
+	// 		{
+	// 			$movie['movie_showings'][1]['showing_datetime'] = strtotime($entry['datum2'].'-14 '.$entry['tijd2']);
+	// 		}
 
-			if(!empty(trim($entry['tijd3'])))
-			{
-				$movie['movie_showings'][2]['showing_datetime'] = strtotime($entry['datum3'].'-14 '.$entry['tijd3']);
-			}
+	// 		if(!empty(trim($entry['tijd3'])))
+	// 		{
+	// 			$movie['movie_showings'][2]['showing_datetime'] = strtotime($entry['datum3'].'-14 '.$entry['tijd3']);
+	// 		}
 
-			$this->Movies_model->insert($movie, FALSE);
-			echo "Inserted '".$movie['movie_name']."' into db. <br />";
-			//echo "<pre>".print_r($movie, true)."</pre>";
+	// 		$this->Movies_model->insert($movie, FALSE);
+	// 		echo "Inserted '".$movie['movie_name']."' into db. <br />";
+	// 		//echo "<pre>".print_r($movie, true)."</pre>";
 
-		}
-	}
+	// 	}
+	// }
 
 	private function get_top($only_can_win = TRUE)
 	{
--- application/views/main/top_xml_output.php
+++ application/views/main/top_xml_output.php
@@ -1,10 +1,25 @@
 <xml version="1.0">
-    <movies>
-<?php foreach($top as $rank => $movie): ?>
-        <movie rank="<?=$rank+1?>">
-            <name><?php echo $movie['grade'] == 'Onbekend' ? 'Nog niet bekend' : $movie['movie_name'] ?></name>
-            <grade><?php echo $movie['grade'] == 'Onbekend' ? '-' : number_format($movie['grade'], 1) ?></grade>
-        </movie>
-<?php endforeach; ?>
-    </movies>
+<lists>
+    <list id="publieksprijs">
+        <movies>
+        <?php foreach($top as $rank => $movie): ?>
+                <movie rank="<?=$rank+1?>">
+                    <name><?php echo $movie['grade'] == 'Onbekend' ? 'Nog niet bekend' : $movie['movie_name'] ?></name>
+                    <grade><?php echo $movie['grade'] == 'Onbekend' ? '-' : number_format($movie['grade'], 1) ?></grade>
+                </movie>
+        <?php endforeach; ?>
+        </movies>
+    </list>
+    <list id="barometer">
+        <movies>
+        <?php foreach($barometer as $rank => $movie): ?>
+                <movie rank="<?=$rank+1?>">
+                    <name><?php echo $movie['grade'] == 'Onbekend' ? 'Nog niet bekend' : $movie['movie_name'] ?></name>
+                    <grade><?php echo $movie['grade'] == 'Onbekend' ? '-' : number_format($movie['grade'], 1) ?></grade>
+                </movie>
+        <?php endforeach; ?>
+        </movies>
+    </list>
+</lists>
+
 </xml>
