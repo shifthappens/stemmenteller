15c68120676257f351a58bd3bbc6a074909e001e
Coen <301815+shifthappens@users.noreply.github.com>
2015-07-08 00:29:37 +0200

    starting to implement CSV movie importer for general public use. Moved importer from Rankings to Admin controller.

--- application/views/admin/sidenav.php
+++ application/views/admin/sidenav.php
@@ -5,4 +5,5 @@
             <?php endif; ?>
             <li class="<?php echo $this->uri->segment(2) == 'votes' ? 'active' : ''; ?>"><a href="admin/votes">Stemuitslagen</a></li>
             <li class="<?php echo $this->uri->segment(2) == 'export' ? 'active' : ''; ?>"><a href="admin/export">Exporteren</a></li>
+            <li class="<?php echo $this->uri->segment(2) == 'import' ? 'active' : ''; ?>"><a href="admin/import">Importeren</a></li>
           </ul>
