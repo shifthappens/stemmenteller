ed491c52e6ba3055a281a8e120fc7356cb60d6fe
Coen <301815+shifthappens@users.noreply.github.com>
2015-07-15 14:19:22 +0200

    adding more security for the manager account

--- application/views/admin/sidenav.php
+++ application/views/admin/sidenav.php
@@ -4,6 +4,8 @@
             <li class="<?php echo $this->uri->segment(2) == 'movies' ? 'active' : ''; ?>"><a href="admin/movies">Films</a></li>
             <?php endif; ?>
             <li class="<?php echo $this->uri->segment(2) == 'votes' ? 'active' : ''; ?>"><a href="admin/votes">Stemuitslagen</a></li>
+            <?php if($this->session->userdata('user_level') == '0'): ?>
             <li class="<?php echo $this->uri->segment(2) == 'export' ? 'active' : ''; ?>"><a href="admin/export">Exporteren</a></li>
             <li class="<?php echo strpos($this->uri->segment(2), 'import') !== FALSE ? 'active' : ''; ?>"><a href="admin/import">Importeren</a></li>
+       		<?php endif; ?>
           </ul>
